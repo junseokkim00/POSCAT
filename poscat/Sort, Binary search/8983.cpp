4d961adf975c02cc5a8a9b60dbdc87563b0e12ba
junseok Kim <junseokkim00@berkeley.edu>
2023-03-21 16:55:31 +0900

    solve 8983

--- poscat/Sort, Binary search/8983.cpp
+++ poscat/Sort, Binary search/8983.cpp
@@ -6,22 +6,43 @@
 using namespace std;
 
 int main() {
-    int m,n,l;
+    int m,n;
+    long long l;
     int answer = 0;
-    vector <int> v;
+    vector <long long> v;
     cin >> m >> n >> l;
     for(int i=0; i<m;i++) {
-        int val;
+        long long val;
         cin >> val;
         v.push_back(val);
     }
     sort(v.begin(), v.end());
+
     for(int i=0;i<n;i++) {
-        int x,y;
+        long long x,y;
         cin >> x >> y;
-        // binary search on every coordinates
-        // 
+        long long lower = x-l+y;
+        long long upper = x+l-y;
+        if (y > l) {
+            continue;
+        } else {
+            int left = 0;
+            int right = v.size()-1;
+
+            while (left <= right) {
+                int mid = (left+right) / 2;
+                if(lower <= v[mid] && v[mid] <= upper) {
+                    answer++;
+                    break;
+                } else if (v[mid] < lower) {
+                    left = mid+1;
+                } else { // upper < v[mid]
+                    right = mid-1;
+                }
+            }
+        }
     }
+    cout << answer;
     
     
 }
