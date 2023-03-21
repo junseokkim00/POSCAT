a4ceecae92da89d75fc149c2259335d4b0986021
Junseok Kim <junseokkim00@gmail.com>
2023-03-21 15:29:00 +0900

    Figuring out in 8983

---
+++ poscat/Sort, Binary search/8983.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int m,n,l;
+    int answer = 0;
+    vector <int> v;
+    cin >> m >> n >> l;
+    for(int i=0; i<m;i++) {
+        int val;
+        cin >> val;
+        v.push_back(val);
+    }
+    sort(v.begin(), v.end());
+    for(int i=0;i<n;i++) {
+        int a,b;
+        cin >> a >> b;
+        // binary search on every coordinates
+    }
+    
+    
+}
