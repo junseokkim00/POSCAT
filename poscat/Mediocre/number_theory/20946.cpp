50b26e6f6c97f533261155a5d0a76f8a380b154d
Junseok Kim <junseokkim00@gmail.com>
2023-03-22 15:03:53 +0900

    add 20946

--- poscat/Mediocre/number_theory/20946.cpp
+++ poscat/Mediocre/number_theory/20946.cpp
@@ -5,7 +5,7 @@ int main() {
     unsigned long long N;
     cin >> N;
     while(N>1) {
-        for(unsigned long long i=2;i<=n;i++) {
+        for(unsigned long long i=2;i<=N;i++) {
             if(n%i==0) {
                 cout << i << '\n';
                 n/=i;
